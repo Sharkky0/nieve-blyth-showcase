a0a0f93234b197cfa6bf802b49134faa51d5f5e4
Luke Gardner <lukegardner09@icloud.com>
2026-06-25 21:56:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }), // Important
+  ],
 });
